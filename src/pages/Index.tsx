5c520dcfadfdf8ebc9d43f0819692b6d39facf6e
anastazess <matthewsakurow@gmail.com>
2025-06-24 15:10:10 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -132,20 +132,20 @@ const Index = () => {
 
 {/* Content - back to right side */}
 <div className="absolute right-16 top-1/2 transform -translate-y-80 text-right">
-  <div className="text-white font-black text-8xl mb-4 tracking-wider">
+  <div className="text-white font-black text-9xl mb-4 tracking-wider">
     MVP OF
   </div>
   <div className="text-white font-black text-3xl mb-2 opacity-90 tracking-wide">
     {tournamentName}
   </div>
-  <div className="text-white/60 font-black text-3xl mb-16 tracking-wide">
+  <div className="text-white/60 font-black text-3xl mb-20 tracking-wide">
     {tournamentName}
   </div>
   
   <div className="text-white font-black text-8xl mb-4 tracking-wider">
     {rating}
   </div>
-  <div className="text-cyan-400 font-bold text-2xl mb-16 tracking-wider">
+  <div className="text-cyan-400 font-bold text-2xl mb-20 tracking-wider">
     RATING
   </div>
   
