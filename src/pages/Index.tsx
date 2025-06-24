be5c3c56d3fb566e14996001a41bc4c4166632b3
anastazess <matthewsakurow@gmail.com>
2025-06-24 14:22:57 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -111,7 +111,7 @@ const Index = () => {
               >
                 {/* Background MVP Text */}
                 <div className="absolute inset-0 flex items-center justify-start pl-4">
-                  <div className="text-white text-[120px] leading-none tracking-wider select-none">
+                  <div className="text-white/5 font-black text-[120px] leading-none tracking-wider select-none">
                     MVP<br/>MVP<br/>MVP
                   </div> 
                 </div>
@@ -146,13 +146,13 @@ const Index = () => {
 
                 {/* Content - back to right side */}
                 <div className="absolute right-8 top-1/2 transform -translate-y-1/2 text-right">
-                  <div className="text-white font-black text-9xl mb-2 tracking-wider">
+                  <div className="text-white font-black text-6xl mb-2 tracking-wider">
                     MVP OF
                   </div>
-                  <div className="text-white font-bold text-1xl mb-1 opacity-90 tracking-wide">
+                  <div className="text-white font-bold text-2xl mb-1 opacity-90 tracking-wide">
                     {tournamentName}
                   </div>
-                  <div className="text-white/60 font-bold text-1xl mb-6 tracking-wide">
+                  <div className="text-white/60 font-bold text-2xl mb-6 tracking-wide">
                     {tournamentName}
                   </div>
                   
