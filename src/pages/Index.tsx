1a676bd2e8385bd4b178c35c9b21248114f589aa
anastazess <matthewsakurow@gmail.com>
2025-06-24 15:23:06 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -145,7 +145,7 @@ const Index = () => {
   <div className="text-white font-black text-[120px] mb-4 tracking-wider">
     {rating}
   </div>
-  <div className="text-cyan-400/75 font-bold text-4xl mb-8 tracking-wider">
+  <div className="text-cyan-400/75 font-bold text-4xl mb-8 tracking-widerу -mt-2">
     RATING
   </div>
   
