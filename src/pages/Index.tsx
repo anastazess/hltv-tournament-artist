d097034a55985729221947207a05f9e06c3231e0
anastazess <matthewsakurow@gmail.com>
2025-06-24 14:46:40 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -112,7 +112,7 @@ const Index = () => {
                 {/* Background MVP Text */}
                 <div className="absolute inset-0 flex items-center justify-start pl-4">
                   <div className="text-white/35 font-black text-[120px] leading-none tracking-wider select-none">
-                    MVP<br/>MVP<br/>MVP
+                    MVP<br/>MVP<br/>MVP<br/>MVP
                   </div> 
                 </div>
                 {/*10 font-black */}
@@ -130,7 +130,7 @@ const Index = () => {
     src={playerImageUrl} 
     alt="Player"
     className="h-full w-full object-contain object-center" // Заменил object-cover на object-contain
-    style={{ transform: "translateX(30%) scale(1.5) translateY(13%)" }} // Объединил трансформации
+    style={{ transform: "translateX(15%) scale(1.5) translateY(13%)" }} // Объединил трансформации
     onError={(e) => {
       const target = e.target as HTMLImageElement;
       target.src = 'https://img-cdn.hltv.org/playerbodyshot/Xkqvuwl9o12Mi20Vd0lzHl.png?ixlib=java-2.1.0&w=400&s=f64d118affc3f2fbadcebf861d70400d';
