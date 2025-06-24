8e618d9bbdb7f26c86cdcf657c41f36737ac0077
anastazess <matthewsakurow@gmail.com>
2025-06-24 15:30:34 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,9 +10,9 @@ import html2canvas from 'html2canvas';
 
 const Index = () => {
   const [tournamentName, setTournamentName] = useState('BLAST.TV AUSTIN MAJOR 2025');
-  const [playerName, setPlayerName] = useState('ZYWOO');
+  const [playerName, setPlayerName] = useState('KARRIGAN');
   const [rating, setRating] = useState('1.42');
-  const [playerImageUrl, setPlayerImageUrl] = useState('https://img-cdn.hltv.org/playerbodyshot/Xkqvuwl9o12Mi20Vd0lzHl.png?ixlib=java-2.1.0&w=400&s=f64d118affc3f2fbadcebf861d70400d');
+  const [playerImageUrl, setPlayerImageUrl] = useState('https://img-cdn.hltv.org/playerbodyshot/7xNUYJWQ2UD1WYEMJfAp2a.png?ixlib=java-2.1.0&w=400&s=11c47c21f9b16abaa29adaca460a575c');
   
   const cardRef = useRef<HTMLDivElement>(null);
 
@@ -122,7 +122,7 @@ const Index = () => {
     src={playerImageUrl} 
     alt="Player"
     className="h-full w-full object-contain object-center"
-    style={{ transform: "translateX(4%) scale(1.4) translateY(14%)" }}
+    style={{ transform: "translateX(2%) scale(1.4) translateY(14%)" }}
     onError={(e) => {
       const target = e.target as HTMLImageElement;
       target.src = 'https://img-cdn.hltv.org/playerbodyshot/Xkqvuwl9o12Mi20Vd0lzHl.png?ixlib=java-2.1.0&w=400&s=f64d118affc3f2fbadcebf861d70400d';
