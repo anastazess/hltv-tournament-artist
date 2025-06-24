860cfe2137f65cdcf28265592e97b28c7293669f
anastazess <matthewsakurow@gmail.com>
2025-06-24 14:31:20 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -125,14 +125,14 @@ const Index = () => {
                 </div> */}
 
                 {/* Player Image - moved to left side */}
-                <div className="absolute left-0 top-0 h-full w-full max-w-[120%] overflow-visible transform -translate-x-1/4">
+                <div className="absolute left-0 top-0 h-full w-full max-w-[90%] overflow-visible transform -translate-x-1/4">
                 <img 
     src={playerImageUrl} 
     alt="Player"
-    className="h-full w-full object-cover object-center scale-125"
+    className="h-full w-full object-cover object-center scale-90"
     onError={(e) => {
       const target = e.target as HTMLImageElement;
-      target.src = 'https://images.unsplash.com/photo-1511512578047-dfb367046420?w=400&h=600&fit=crop';
+      target.src = 'https://img-cdn.hltv.org/playerbodyshot/nbkBkY04pLo_1P6b59hVxy.png?ixlib=java-2.1.0&w=400&s=8a0e930e1585977f71c44fabfe3ddac1';
     }}
   />
 </div>
